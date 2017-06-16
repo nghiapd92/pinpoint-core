c055d10735685fef3048be5d11945121284ad028
Phạm Đại Nghĩa <nghiapd@vtc.vn>
2017-06-16 18:09:57 +0700

    fix some bug in SuccessResponse and ErrorResponse

--- response/ErrorResponse.js
+++ response/ErrorResponse.js
@@ -2,8 +2,9 @@ let Response = require("./Response");
 
 module.exports = class ErrorResponse extends Response{
 	constructor(_error){
+		super();
 		let error =  Exception.parse(_error);
-		
+
 		this.code = error.code;
 		this.message = error.message;
 		this.response = error.responseObject;
--- response/SuccessResponse.js
+++ response/SuccessResponse.js
@@ -2,6 +2,8 @@ let Response = require("./Response");
 
 module.exports = class SuccessResponse extends Response{
 	constructor(message, responseObject){
+		super();
+		
 		if(message) this.message = message;
 		if(responseObject) this.response = responseObject;
 
